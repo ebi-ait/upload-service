8e8073248abd2f9e480a41c784cb360edd75bca7
Rodrey Goite <rodrey@ebi.ac.uk>
2020-02-03 09:53:01 +0000

    Added a copy of variables.tf in _template.

---
+++ terraform/envs/_template/variables.tf
@@ -0,0 +1,111 @@
+variable "deployment_stage" {
+  type = "string"
+}
+
+variable "aws_profile" {
+  type = "string"
+}
+
+variable "slack_webhook" {
+  type = "string"
+}
+
+// VPC
+
+variable "vpc_cidr_block" {
+  type = "string"
+}
+
+// S3
+
+variable "bucket_name_prefix" {
+  type = "string"
+}
+
+variable "staging_bucket_arn" {
+  type = "string"
+}
+
+
+// DNS
+
+variable "parent_zone_domain_name" {
+  type = "string"
+}
+
+variable "upload_api_fqdn" {
+  type = "string"
+}
+
+// API Lambda
+
+variable "ingest_api_key" {
+  type = "string"
+}
+
+// API Gateway
+
+variable "upload_api_api_gateway_id" {
+  type = "string"
+}
+
+// Checksum Lambda
+
+variable "csum_docker_image" {
+  type = "string"
+}
+
+// Batch
+
+variable "validation_cluster_ec2_key_pair" {
+  type = "string"
+}
+variable "validation_cluster_ami_id" {
+  type = "string"
+}
+variable "validation_cluster_instance_type" {
+  type = "string"
+}
+variable "validation_cluster_min_vcpus" {
+  type = "string"
+}
+variable "csum_cluster_ec2_key_pair" {
+  type = "string"
+}
+variable "csum_cluster_instance_type" {
+  type = "string"
+}
+variable "csum_cluster_min_vcpus" {
+  type = "string"
+}
+
+// RDS
+
+variable "db_username" {
+  type = "string"
+}
+variable "db_password" {
+  type = "string"
+}
+variable "db_instance_count" {
+  type = "string"
+}
+variable "preferred_maintenance_window" {
+  type = "string"
+}
+
+// DCP Ingest
+
+variable "ingest_api_host" {
+  type = "string"
+}
+
+// AUTH
+
+variable "dcp_auth0_audience" {
+  type = "string"
+}
+
+variable "gcp_service_acct_creds" {
+  type = "string"
+}
